dc8d9978352090824862ce2151977d88341cb1fd
karinaschneider1980-maker <karinaschneider1980@gmail.com>
2026-03-21 21:32:19 +0100

    added API in JS, changed CSS and added bonus in html

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-function generateTravel(event) {
-  event.preventDefault()
-
+function displayTravel(response) {
   let resultsElement = document.querySelector(".results-cards")
-
-  const tips = [
-    "1. The 1st top must visit place",
-    "2. The 2nd top must visit place",
-    "3. The 3rd top must visit place",
-    "4. The 4th top must visit place",
-    "5. The 5th top must visit place",
-    "Bonus: Explore the hidden gems of the city",
-  ]
-
   resultsElement.innerHTML = ""
+
+  let answer = response.data.answer
+  answer = answer.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
+  answer = answer.replace(/###/g, "")
+  answer = answer.replace(/\d\./g, "<br /><br />$&")
+  answer = answer.replace(/Bonus/g, "<br /><br /><strong>Bonus</strong>")
 
   const typewriter = new Typewriter(resultsElement, {
     autoStart: false,
-    delay: 40,
+    delay: 20,
     cursor: "",
     loop: false,
   })
 
-  tips.forEach((tip) => {
-    typewriter.typeString(tip).typeString("<br />").pauseFor(250)
-  })
+  typewriter.typeString(answer).start()
+}
+function generateTravel(event) {
+  event.preventDefault()
 
-  typewriter.start()
+  let apiKey = "d37caee084ftd6c3baf32ae334oca1bf"
+  let cityInput = document.querySelector(".city").value
+  let prompt = `What are the top 5 must visit places in ${cityInput} in South Korea? Also give 1 bonus hidden gem. For each place include the address in hangul`
+  let context =
+    "You are a South Korea travel expert. Give a numbered list of 5 must visit places with a very short description per place."
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+  let resultsElement = document.querySelector(".results-cards")
+  resultsElement.innerHTML = "Loading..."
+
+  axios.get(apiUrl).then(displayTravel)
 }
 
 let travelFormElement = document.querySelector("#travel-form")
